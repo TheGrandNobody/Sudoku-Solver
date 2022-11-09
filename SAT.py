c2663dfee565bd50cc502f1511ccdd725ff002ad
kimjongng88 <84227372+kimjongng88@users.noreply.github.com>
2022-11-09 13:44:18 +0100

    Update SAT.py

--- SAT.py
+++ SAT.py
@@ -1,16 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import sys, os, math
+import numpy as np
+import itertools
+from pulp import *
 
 def read_sudoku_from_file():
     args = sys.argv
-    print(args)
     if len(args) != 2:
-        sys.exit(f"usage: python3 ./{args[0]} input_sudoku_file.txt")       
+        sys.exit(f"usage: python3 ./{args[0]} input_sudoku_file.txt")
+
     try:
         with open(args[1], "r") as f:
             sudoku_in = []
             for line in f.readlines():
-                line = ' '.join(line)
-                line = line.replace('.', '0')
                 #converting string line from file to integer list
                 int_line = list(map(lambda t: int(t), line.replace('\n', '').split()))
                 print(int_line)
@@ -19,30 +23,28 @@ def read_sudoku_from_file():
     except Exception as e:
         sys.exit(e)
 
-def num_to_cnf(cnf_var, num, invert):
-    """
-    convert number to cnf clause
-    num: 5 -> 0101
-    cnf_var: 1 2 3 4
-    ----------------
-    new_clause: -1 2 -3 4
-    if invert = True -> 1 -2 3 -4 (replace all 0 with 1)
-    """
+
+def num_to_cnff(p,num, invert):
     s = -1 if invert else 1
-    #list of binary digits from num, same length as cnf_var
-    bin_list = map(lambda t: int(t), bin(num)[2:].zfill(len(cnf_var)))
-    new_clause = [*map(lambda t: s*t[0] if t[1] else -s*t[0], zip(cnf_var, bin_list))]
-    return new_clause
+    clause = s * int(str(p)+str(num))
+    return clause
+
+def constraint(a, j, i, num, row):
+    if row:
+        clause_2 = -1 * int(str(i) + str(a) + str(num))
+        clause_1 = -1 * int(str(i) + str(j) + str(num))
+    else:
+        clause_2 = -1 * int(str(a) + str(i) + str(num))
+        clause_1 = -1 * int(str(j) + str(i) + str(num))
+    return [(clause_1, clause_2)]#[clause_1, clause_2]
+
+def constraint_box(i,j,a,d,num):
+    clause_1 = -1 * int(str(i) + str(j) + str(num))
+    clause_2 = -1 * int(str(a) + str(d) + str(num))
+
+    return [(clause_1, clause_2)]
 
 
-#add constraint: p and q must be different
-def print_different_num_cnf(fp, p, q, n):
-    for num in range(1, n+1):
-        for p1 in num_to_cnf(p, num, True):
-            fp.write(f"{p1} ")
-        for q1 in num_to_cnf(q, num, True):
-            fp.write(f"{q1} ")
-        fp.write("0\n")
 
 #write at the beginning of file
 def insert(originalfile, string):
@@ -52,83 +54,140 @@ def insert(originalfile, string):
             f2.write(f.read())
     os.rename('newfile.txt', originalfile)
 
-
 def make_cnf_dimacs(sudoku_in, out_file):
-    print(sudoku_in)
     clauses_number = 0
     #dimension of puzzle and minimal number of bits to represent one number
     n = len(sudoku_in[0])
     block = int(math.sqrt(n))
     num_of_bits = len(bin(n)[2:])
 
-    with open(out_file, "w") as f:
-        for i in range(1, n*n+1):
-            #LOG CODING: each number is represented with n-bits (num_of_bits) pn pn-1 ... p1
-            p = [num_of_bits*i - (num_of_bits-1) + offset for offset in range(num_of_bits)]
+    '''
+    Defining Rows, Cols and Values of Sudoku Board
+    '''
+    numbers = [str(i) for i in range(1, n + 1)]
+    Rows = numbers
+    Cols = numbers
+    Values = numbers
 
-            #NOTE: CONSTRAINT 1
-            #valid numbers in sudoku: from 1 to n
-            #restrict all other numbers that can be produced with our coding
-            forbidden_numbers = [*range(n+1, 1 << num_of_bits)]
-            forbidden_numbers.append(0)
-            for num in forbidden_numbers:
-                for j in num_to_cnf(p, num, True):
-                    f.write(f"{j} ")
-                clauses_number += 1
+    
+
+    Subgrids =[]
+    for i in range(block):
+        for j in range(block):
+            Subgrids += [[(int(Rows[block*i+k]),int(Cols[block*j+l])) for k in range(3) for l in range(3)]]
+
+    with open(out_file, "w") as f:
+            
+        for r in Rows:
+            for c in Cols:
+                for v in Values:
+                    a = int(str(r) + str(c) + str(v))
+                    f.write(f"{a} ")
                 f.write("0\n")
 
-            #NOTE: CONSTRAINT 2
-            #add numbers from given input sudoku
-            #converting serial number (1-n*n) to index (i, j)
-            curr_num = sudoku_in[(i-1)//n][(i-1)%n]
-            if curr_num != 0:
-                #add all non-zero numbers
-                for j in num_to_cnf(p, curr_num, False):
-                    f.write(f"{j} 0\n")
+        for i in range(1, n+1):
+            for j in range (1, n+1): 
+                p = int(str(i)+str(j))
+
+                #NOTE: CONSTRAINT 1
+                #add numbers from given input sudoku
+                #converting serial number (1-n*n) to index (i, j)
+                curr_num = sudoku_in[i-1][j-1]#[(i-1)//n][(j-1)%n]
+                if curr_num != 0:
+                    #add all non-zero numbers
+                    a = num_to_cnff(p, curr_num, False)
+                    f.write(f"{a} 0\n")
                     clauses_number += 1
+                for b in Subgrids:
+                    for r,c in b:
+                        if i is r and j is c:
+                            for a, d in b:
+                                if curr_num != 0:
+                                    z = constraint_box(i,j,a,d, curr_num)
+                                    for dd, s in z:
+                                        f.write(f"{dd} ")
+                                        f.write(f"{s} ")
+                                        f.write("0\n")
+                                        clauses_number += 1
+                                else:
+                                    for v in Values:
+                                        z = constraint_box(i,j,a,d, v)
+                                        for dd, s in z:
+                                            f.write(f"{dd} ")
+                                            f.write(f"{s} ")
+                                            f.write("0\n")
+                                            clauses_number += 1
+                            break
+                        else:
+                            continue
+                
+                #NOTE: CONSTRAINT 2
+                #Row
+                for a in range(1, n+1):
+                    if a != j & curr_num != 0:
+                        z = constraint(a, j, i,curr_num, True)
+                        for c, d in z:
+                            f.write(f"{c} ")
+                            f.write(f"{d} ")
+                            f.write("0\n")
+                            clauses_number += 1
+                    elif a != j & curr_num == 0:
+                        for v in Values:
+                            z = constraint(a, j, i, v, True)
+                            for c, d in z:
+                                f.write(f"{c} ")
+                                f.write(f"{d} ")
+                                f.write("0\n")
+                                clauses_number += 1
 
-            #NOTE: CONSTRAINT 3
-            #in each row, column and block different numbers from 1 to n
-            for j in range(i+1, n*n+1):
-                #LOG CODING
-                q = [num_of_bits*j - (num_of_bits-1) + offset for offset in range(num_of_bits)]
 
-                #row
-                if (i-1)//n == (j-1)//n:
-                    print_different_num_cnf(f, p, q, n)
-                    clauses_number += n
-                #column
-                elif (i-1)%n == (j-1)%n:
-                    print_different_num_cnf(f, p, q, n)
-                    clauses_number += n
-                #block
-                elif (((i-1)//n)//block, ((i-1)%n)//block) == (((j-1)//n)//block, ((j-1)%n)//block):
-                    print_different_num_cnf(f, p, q, n)
-                    clauses_number += n
+                        
+                #Column
+                for b in range(1, n+1):
+                    if b != j & curr_num != 0:
+                        z = constraint(b, i, j,curr_num, False)
+                        for c, d in z:
+                            f.write(f"{c} ")
+                            f.write(f"{d} ")
+                            f.write("0\n")
+                            clauses_number += 1
+                    elif b != j & curr_num == 0:
+                        for v in Values:
+                            z = constraint(b,i,j, v, True)
+                            for c, d in z:
+                                f.write(f"{c} ")
+                                f.write(f"{d} ")
+                                f.write("0\n")
+                                clauses_number += 1
 
-    insert(out_file, f"p cnf {n*n*num_of_bits} {clauses_number}")
-    return n
+    nn_bits= n*n*num_of_bits
+    return nn_bits
 
-def cnf_to_num(cnf_clause):
-    """
-    Inverse of `num_to_cnf` function
-    convert cnf clause to number
-    from [-1 2 -3 4] to 5
-    """
-    str_bin = [*map(lambda digit: '1' if int(digit) > 0 else '0' , cnf_clause)]
-    return int(''.join(str_bin),2)
+def dupe(out_file, n):
+    lines = open(out_file, 'r').readlines()
+
+    lines_set = set(lines)
+
+    print(len(lines_set))
+
+    out  = open(out_file, 'w')
+
+    for line in lines_set:
+        out.write(line)
+
+    insert(out_file, f"p cnf {n} {len(lines_set)}")
+    print('fini')
+
 
 def main():
     sudoku_in = read_sudoku_from_file()
-    out_file = "out_sudoku2.cnf"
+    out_file = "out_sudoku3.cnf"
 
-    #NOTE: modeling solution
     n = make_cnf_dimacs(sudoku_in, out_file)
-
+    dupe(out_file, n)
 
 
 if __name__ == "__main__":
     main()
 
-
-#python3 SAT.py sudoku/own_test.txt -------->>>>>> Run this command 'sudoku/own_test.txt' should be adjusted to the correct row length, I'm still working on it
+    #python3 solve_sudoku.py sudoku/test.txt
