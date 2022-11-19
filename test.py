c7cb912c807efbe93eb17033b0b35f16728f8a59
kimjongng88 <84227372+kimjongng88@users.noreply.github.com>
2022-11-19 10:58:46 +0100

    Update test.py

--- test.py
+++ test.py
@@ -5,5 +5,12 @@ import sys
 
 if __name__ == "__main__":
     dpll = solver.DPLL()
-    dpll.find_solution(sys.argv[1])
+    dpll.find_solution(sys.argv[1],sys.argv[2])
     print({k: v for k,v in sorted({k: dpll.solution[k] for k in sorted(dpll.solution)}.items(), key=itemgetter(1))})
+
+    #python3 test.py sudoku/sudoku2.cnf 1
+    
+    
+    
+    # 0 for basic
+    # 1 for Jeroslow Wang
