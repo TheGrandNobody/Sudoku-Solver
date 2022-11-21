05aaa1111646f291d5924d10f66e223a536388be
kimjongng88 <84227372+kimjongng88@users.noreply.github.com>
2022-11-21 21:32:08 +0100

    Update test.py

--- test.py
+++ test.py
@@ -6,9 +6,25 @@ import sys
 if __name__ == "__main__":
     dpll = solver.DPLL()
     dpll.find_solution(sys.argv[1],sys.argv[2])
-    print({k: v for k,v in sorted({k: dpll.solution[k] for k in sorted(dpll.solution)}.items(), key=itemgetter(1))})
+    #print({k: v for k,v in sorted({k: dpll.solution[k] for k in sorted(dpll.solution)}.items(), key=itemgetter(1))})
 
-    #python3 test.py sudoku/sudoku2.cnf 1
+    solution_values = [ ]
+    for k,v in sorted({k: dpll.solution[k] for k in sorted(dpll.solution)}.items(), key=itemgetter(1), reverse = True):
+        if v is False:
+            k = "-" + k
+        else:
+            pass
+        solution_values.append(k)
+    out_file = "output.txt"
+    out  = open(out_file, 'w')
+    out.write(f"p cnf {len(solution_values)} {len(solution_values)} \n")
+    for b in solution_values:
+        out.write(f"{b} ")
+        out.write("0\n")
+    print('fini')
+
+
+    #python3 test.py sudoku/sudoku2.cnf 1 sudoku/test.txt
     
     
     
